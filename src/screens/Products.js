d752cc630387ea27f964bca86823098b32ac2470
Berk Bay <berk.bay.ce@gmail.com>
2022-01-26 01:34:05 +0300

    Get API from LocalHost

--- src/screens/Products.js
+++ src/screens/Products.js
@@ -1,8 +1,30 @@
 import {View, Text, TouchableOpacity, StyleSheet} from "react-native";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 const Product = ({navigation}) => {
+    const [product, setProduct] = useState([])
+
+    const getData = () => {
+        axios.get('http://localhost:3000/products')
+            .then((response) => {
+                setProduct(response.data)
+            })
+    }
+
+    useEffect(() => {
+        getData();
+    },[]);
+
+    const arr = product.map((data, index) => {
+        return (
+            <Text key={data.name}>{data.name}</Text>
+        );
+    })
+
     return(
         <View style={styles.container}>
+            {arr}
             <TouchableOpacity onPress={() => navigation.navigate('ProductDetail')}>
                 <Text>Product</Text>
             </TouchableOpacity>
